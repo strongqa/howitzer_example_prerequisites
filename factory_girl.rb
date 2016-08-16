08168df336ad935f40f3120c6e88557e6e37eb02
Roman Parashchenko <romikoops1@gmail.com>
2016-08-16 19:33:24 +0200

    simplified requires

--- factory_girl.rb
+++ factory_girl.rb
@@ -1,6 +1,5 @@
 # For more information about configuration please refer to
 # http://www.rubydoc.info/gems/factory_girl/file/GETTING_STARTED.md
-require 'factory_girl'
 
 FactoryGirl.definition_file_paths = [File.join(__dir__, 'factories')]
 FactoryGirl.find_definitions
