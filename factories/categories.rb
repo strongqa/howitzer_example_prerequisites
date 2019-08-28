958d5839cda9acfe42aa506511d1a305f71dec4a
Yevhen Plakhotnikov <yevhen@strongs-Mac-mini.local>
2019-08-28 16:51:05 +0300

    Updated Category factory

--- factories/categories.rb
+++ factories/categories.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :category do
     name { "Category name #{Gen.serial}" }
+
+    trait :default do
+      name { 'Category name Test' }
+      initialize_with { Category.default('Category name Test') || Category.new }
+    end
   end
 end
--- models/category.rb
+++ models/category.rb
@@ -1,4 +1,8 @@
 require_relative 'base'
 class Category < Base
   has_many :articles
+
+  def self.default(name_cat)
+    where(name: name_cat.to_sym).first
+  end
 end
